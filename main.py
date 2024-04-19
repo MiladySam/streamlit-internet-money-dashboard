0a5c21b7fbd3394a07d3801c8334dfbbd002e091
samuel <samuel.tsfmr@eduge.ch>
2024-04-19 07:27:38 +0200

    remove img

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
 import yfinance as yf
 import streamlit as st
 
+
 from PIL import Image
 from urllib.request import urlopen
 
 st.title("Daily Price")
 st.header("Dashboard")
 
-# Utilise l'API yfinance pour récupérer les données de prix
 
-Bitcoin = yf.Ticker("BTC-USD")
-Ethereum = yf.Ticker("ETH-USD")
-pepecoin = yf.Ticker("PEPE-USD")
-bittensor = yf.Ticker("TAO-USD")  
-dogecoin = yf.Ticker("DOGE-USD")   
+# Définit les cryptomonnaies à suivre
+Bitcoin = "BTC-USD"
+Ethereum = "ETH-USD"
+pepecoin = "PEPE24478-USD"
+bittensor = "TAO22974-USD"
+dogecoin = "DOGE-USD"  
+
+
+# Utilise l'API yfinance pour récupérer les données de prix
+BTC_data = yf.Ticker(Bitcoin)
+ETH_data = yf.Ticker(Ethereum)
+PEPE_data = yf.Ticker(pepecoin)
+TAO_data = yf.Ticker(bittensor)
+DOGE_data = yf.Ticker(dogecoin)
+
+
+
 
 # Récupère l'historique des prix pour chaque cryptomonnaie
-Bitcoin_data = Bitcoin.history(period="1d")
-Ethereum_data = Ethereum.history(period="1d")
-pepe_data = pepecoin.history(period="1d")
-tao_data = bittensor.history(period="1d")
-doge_data = dogecoin.history(period="1d")
+BTC_hist = BTC_data.history(period="max")
+ETH_hist = ETH_data.history(period="max")
+PEPE_hist = PEPE_data.history(period="max")
+TAO_hist = TAO_data.history(period="max")
+DOGE_hist = DOGE_data.history(period="max")
 
 
+
+# Récupère les données de prix pour chaque cryptomonnaie pour la date du 01 avril 2024
 BTC = yf.download(Bitcoin, start="2024-04-01", end="2024-04-01")
 ETH = yf.download(Ethereum, start="2024-04-01", end="2024-04-01")
 DOGE = yf.download(dogecoin, start="2024-04-01", end="2024-04-01")
 PEPE = yf.download(pepecoin, start="2024-04-01", end="2024-04-01")
 TAO = yf.download(bittensor, start="2024-04-01", end="2024-04-01")
+
+
+# Affiche les données de prix
+st.write("Bitcoin")
+
